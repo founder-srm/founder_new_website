34800fcbb75f428844a1ee728cac9c9db903d9f2
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-02-07 00:04:36 +0530

    first commit to teams page

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Page() {
     return (
-        <main className="w-screen h-full flex flex-col bg-[#0E0E0E]">
+        <main className="w-screen h-full flex flex-col bg-[#0E0E0E] overflow-auto">
             <section>
                 {/* Add a hero image section here */}
             </section>
-            <section className=" grid">
+            <section className=" grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2">
                 
             </section>
 

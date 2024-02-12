9235300704ce441afd63de0e418ecce15a2e8530
Suvan Gowri Shanker <118198968+greeenboi@users.noreply.github.com>
2024-02-12 12:56:19 +0000

    added component under hero section

--- app/page.tsx
+++ app/page.tsx
@@ -38,9 +38,29 @@ export default function Home() {
             <TypingEffect text={text} />
           </div>
         </section>
-        <div className="w-screen h-screen grid grid-rows-2 md:grid-cols-2 mt-9">
-          <div className="w-full h-full colors md:h-screen"></div>
-          <div className="w-full h-full colors md:h-screen "></div>
+        <div className="w-screen h-screen bg-[#0E0E0E] flex flex-col mt-9">
+          <div className="w-full h-full flex flex-row justify-between items-center md:px-8 colors md:h-screen">
+            <video
+              width="320"
+              height="240"
+              className=" w-[500px] h-[500px] overflow-hidden object-cover "
+              autoPlay
+              muted
+              loop={true}
+              src="/chips.webm"
+            >
+              Your browser does not support the video tag.
+            </video>
+            <div className =" flex flex-col my-4 mx-12">
+              <h2 className='font-mono font-semibold text-xl md:text-3xl text-white'>
+                Build a Startup!
+              </h2>
+              <p className=" font-mono font-normal text-lg text-white">
+                Work With us to 
+              </p>
+            </div>
+          </div>
+          {/* <div className="w-full h-full colors md:h-screen "></div> */}
         </div>
       </FadeIn>
     </>

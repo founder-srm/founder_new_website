b6eed9055cede530886ad4d9746e5c3a3a5f1597
Suvan Gowri Shanker <118198968+greeenboi@users.noreply.github.com>
2024-02-14 08:55:37 +0000

    evednts and reg page changes

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -16,20 +16,31 @@ import {
     DrawerContent,
     DrawerDescription,
     DrawerHeader,
+    DrawerFooter,
     DrawerTitle,
     DrawerTrigger,
 } from "@/components/ui/drawer"
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+  } from "@/components/ui/resizable";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+
 import Navbar from "../Navbar";
+import { XCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 
 interface EventList {
     id: string;
     name: string;
     description: string;
     venue: string;
-    start_date: string;
-    end_date: string;
+    start_date: Date;
+    end_date: Date;
     updated_at: string;
     banner_url: string;
     moreinfo: string;
@@ -38,6 +49,8 @@ interface EventList {
 export default function Page() {
     const [events, setEvents] = useState<EventList[]>([]);
     const [loading, setLoading] = useState(true);
+
+    const router = useRouter();
 
     useEffect(() => {
         const fetchEvents = async () => {
@@ -66,6 +79,22 @@ export default function Page() {
         numColumns = 3;
     }
     
+    function convertTimestampToDateAndTime(timestamp : Date) {
+        const date = new Date(timestamp);
+        const year = date.getFullYear();
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const month = monthNames[date.getMonth()]; // Months are 0-based in JavaScript
+        const day = date.getDate();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+    
+        // Format the date and time
+        const formattedDate = `${day} ${month} ${year}`;
+        const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    
+        return `${formattedDate} ${formattedTime}`;
+    }
 
     return (
         <main className="w-screen h-screen flex flex-col overflow-auto bg-[#090909]">
@@ -84,20 +113,47 @@ export default function Page() {
                                         <Button variant={"ghost"} className="text-black rounded-3xl flex bg-[#6349f8] mx-auto border border-[#9289f2]">Show More</Button>
                                     </DrawerTrigger>
                                     
-                                    <DrawerContent className="bg-gray-400 flex flex-col gap-3 h-3/5 overflow-scroll" style={{scrollbarWidth: "thin", scrollbarColor: "rgba(255, 255, 255, 0.5) transparent" }}>
-                                        <div className="flex justify-end">
-                                            <DrawerClose asChild>
-                                                <Button variant="outline" className="flex justify-center mr-10 bg-[#555555] text-white ">Close</Button>
-                                            </DrawerClose>
-                                        </div>
-                                        <div>
+                                    <DrawerContent className="bg-[#090909] flex flex-col gap-3 h-fit">
                                             <DrawerHeader>
-                                                <DrawerTitle className="text-3xl mt-5 mb-8 flex justify-center">{event.name}</DrawerTitle>
+                                                <DrawerTitle className="text-3xl mt-5 mb-4 flex justify-center text-white">{event.name}</DrawerTitle>
                                             </DrawerHeader>
-                                            <DrawerDescription className="flex items-center justify-center text-center text-black">{event.description}</DrawerDescription>
-                                            <DrawerDescription className="m-5 flex items-center justify-center text-black">VENUE: {event.venue}</DrawerDescription>
-                                            <DrawerDescription className="m-5 flex items-center justify-center text-black">START DATE: {event.start_date}</DrawerDescription>
-                                        </div>
+                                            <DrawerDescription className="flex items-center justify-center text-center text-white">
+                                                <ResizablePanelGroup
+                                                    direction="horizontal"
+                                                    className=" max-w-5xl rounded-lg border"
+                                                >
+                                                    <ResizablePanel defaultSize={50}>
+                                                        <div className="flex h-[200px] items-center justify-center p-6">
+                                                        <span className="font-semibold">{event.description}</span>
+                                                        </div>
+                                                    </ResizablePanel>
+                                                    <ResizableHandle />
+                                                    <ResizablePanel defaultSize={50}>
+                                                        <ResizablePanelGroup direction="vertical">
+                                                        <ResizablePanel defaultSize={25}>
+                                                            <div className="flex h-full items-center justify-center p-6">
+                                                            <span className="font-semibold">VENUE: <Badge variant="secondary" className="">{event.venue}</Badge></span>
+                                                            </div>
+                                                        </ResizablePanel>
+                                                        <ResizableHandle />
+                                                        <ResizablePanel defaultSize={75}>
+                                                            <div className="flex flex-col h-full items-center justify-center p-6">
+                                                                <span className="font-semibold">START DATE: {convertTimestampToDateAndTime(event.start_date)}</span>
+                                                                <span className="font-semibold">End DATE: {convertTimestampToDateAndTime(event.end_date)}</span>
+                                                            </div>
+                                                        </ResizablePanel>
+                                                        </ResizablePanelGroup>
+                                                    </ResizablePanel>
+                                                </ResizablePanelGroup>
+                                            </DrawerDescription>
+                                            <DrawerFooter className="flex flex-row gap-4 mx-2">
+                                                <Button className="text-black flex-1" variant='secondary'  onClick={() => router.push(`/events/register/${event.name}`)}>
+                                                    Register Now
+                                                </Button>
+                                                <DrawerClose asChild>
+                                                    <Button variant="destructive" className="flex flex-1 justify-center mr-10 bg-[#555555] text-white "><XCircle /></Button>
+                                                </DrawerClose>
+                                            </DrawerFooter>
                                     </DrawerContent>
                                 </Drawer>
                             </CardHeader>
--- app/events/register/[slug]/page.tsx
+++ app/events/register/[slug]/page.tsx
@@ -1,10 +1,17 @@
+'use client'
+import Navbar from "@/app/Navbar";
 import Link from "next/link";
+import { useParams } from "next/navigation";
+import { string } from "zod";
 
 export default function Page() {
+    const params = useParams<{ tag: string; item: string }>();
+
+    console.log(params);
     return (
-        <div>
-        <h1>Page</h1>
-        <Link href="/">Home</Link>
-        </div>
+        <main className="flex flex-col w-screen h-full min-h-screen bg-[#090909] text-white">
+            <Navbar/>
+
+        </main>
     );
 }

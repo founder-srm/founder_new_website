cd98b23ebf59ec746a12f30c5ac6d86672978f10
Suvan Gowri Shanker <118198968+greeenboi@users.noreply.github.com>
2024-02-14 22:02:03 +0000

    changed the events page for a better ui and design. added a sample form for the registration page and the types too

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -28,9 +28,11 @@ import {
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { Skeleton } from "@/components/ui/skeleton"
+
 
 import Navbar from "../Navbar";
-import { XCircle } from "lucide-react";
+import { Info, XCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 
@@ -44,6 +46,7 @@ interface EventList {
     updated_at: string;
     banner_url: string;
     moreinfo: string;
+    isfeatured: boolean;
 }
 
 export default function Page() {
@@ -78,87 +81,135 @@ export default function Page() {
     } else if (numEvents === 2 || numEvents === 3) {
         numColumns = 3;
     }
+
+    function ellipsisAfter25Words(text : string) {
+        const words = text.split(' ');
+        if (words.length > 25) {
+            return words.slice(0, 25).join(' ') + '.....';
+        } else {
+            return text;
+        }
+    }
     
     function convertTimestampToDateAndTime(timestamp : Date) {
         const date = new Date(timestamp);
         const year = date.getFullYear();
         const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        const month = monthNames[date.getMonth()]; // Months are 0-based in JavaScript
+        const month = monthNames[date.getMonth()]; 
         const day = date.getDate();
         const hours = date.getHours();
         const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
+        
     
         // Format the date and time
-        const formattedDate = `${day} ${month} ${year}`;
-        const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+        const formattedDate = `${day}${getDaySuffix(day)} of ${month} ${year}`;
+
+        function getDaySuffix(day: number) {
+            if (day >= 11 && day <= 13) {
+                return "th";
+            }
+
+            switch (day % 10) {
+                case 1:
+                    return "st";
+                case 2:
+                    return "nd";
+                case 3:
+                    return "rd";
+                default:
+                    return "th";
+            }
+        }
+        const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${hours >= 12 ? 'pm' : 'am'}`;
     
-        return `${formattedDate} ${formattedTime}`;
+        return `${formattedTime} on the ${formattedDate}`;
     }
 
     return (
-        <main className="w-screen h-screen flex flex-col overflow-auto bg-[#090909]">
+        <main className="w-screen h-screen flex flex-col justify-start overflow-auto bg-[#090909]">
             <Navbar />
-            <section className="flex-grow py-6">
+            <section className="flex flex-col justify-start ">
                 <h1 className=" text-center text-white font-bold text-4xl mt-5 mb-10 py-5">Events</h1>
-                <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-${numColumns} gap-5 md:gap-8 lg:gap-10 px-4`}>
-                    {events && events.map((event, index) => (
-                        <Card key={index} className="font-medium bg-[#4837ff] text-gray-100">
-                            <CardHeader className="flex flex-col justify-between h-80">
-                                <CardTitle className="p-5 flex items-center justify-center border rounded-xl border-gray-300">{event.name}</CardTitle>
-                                <CardDescription className="mb-0 mt-auto text-white flex items-end font-normal">{event.venue}</CardDescription>
-                                
-                                <Drawer>
-                                    <DrawerTrigger asChild>
-                                        <Button variant={"ghost"} className="text-black rounded-3xl flex bg-[#6349f8] mx-auto border border-[#9289f2]">Show More</Button>
-                                    </DrawerTrigger>
-                                    
-                                    <DrawerContent className="bg-[#090909] flex flex-col gap-3 h-fit">
-                                            <DrawerHeader>
-                                                <DrawerTitle className="text-3xl mt-5 mb-4 flex justify-center text-white">{event.name}</DrawerTitle>
-                                            </DrawerHeader>
-                                            <DrawerDescription className="flex items-center justify-center text-center text-white">
-                                                <ResizablePanelGroup
-                                                    direction="horizontal"
-                                                    className=" max-w-5xl rounded-lg border"
-                                                >
-                                                    <ResizablePanel defaultSize={50}>
-                                                        <div className="flex h-[200px] items-center justify-center p-6">
-                                                        <span className="font-semibold">{event.description}</span>
-                                                        </div>
-                                                    </ResizablePanel>
-                                                    <ResizableHandle />
-                                                    <ResizablePanel defaultSize={50}>
-                                                        <ResizablePanelGroup direction="vertical">
-                                                        <ResizablePanel defaultSize={25}>
-                                                            <div className="flex h-full items-center justify-center p-6">
-                                                            <span className="font-semibold">VENUE: <Badge variant="secondary" className="">{event.venue}</Badge></span>
+                <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-${numColumns} gap-5 md:gap-8 lg:gap-10 mx-2 md:mx-24`}>
+                    {loading? ( 
+                        Array.from({ length: 6 }).map((_, index) => (
+                            <div key={index} className="flex flex-col space-y-3">
+                                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                                <div className="space-y-2">
+                                    <Skeleton className="h-4 w-[250px]" />
+                                    <Skeleton className="h-4 w-[200px]" />
+                                </div>
+                            </div>
+                        ))
+                    ):(
+                        events && events.map((event, index) => ( 
+                            <Card key={index} className={`font-medium bg-[#4837ff] h-fit w-full ${ event.isfeatured ? 'col-span-full' : ''} text-gray-100 shadow-md shadow-[#8E2DE2]`} style={{ backgroundImage: `url(${event.banner_url})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                                <CardHeader className="flex flex-col justify-between h-fit">
+                                    <CardTitle className="p-5 flex items-center justify-center border rounded-xl border-gray-300 backdrop-blur-md">{event.name}</CardTitle>
+                                    <CardDescription className="mx-4  flex items-end font-normal backdrop-blur-md border border-white border-dashed rounded-md">
+                                        <p className="text-white text-center w-full">
+                                            {ellipsisAfter25Words(event.description)}
+                                        </p>
+                                    </CardDescription>
+                                    <Drawer>
+                                        <DrawerTrigger asChild>
+                                            <Button variant={"ghost"} className="text-white rounded-3xl flex bg-[#6349f8] bg-opacity-45 backdrop-blur-md mx-auto border border-[#9289f2]">Know More</Button>
+                                        </DrawerTrigger>
+                                        
+                                        <DrawerContent className="bg-[#0E0E0E] flex flex-col gap-3 h-fit">
+                                                <DrawerHeader>
+                                                    <DrawerTitle className="text-3xl mt-5 mb-4 flex justify-center text-white">{event.name}</DrawerTitle>
+                                                </DrawerHeader>
+                                                <DrawerDescription className="flex items-center justify-center text-center text-white">
+                                                    <ResizablePanelGroup
+                                                        direction="horizontal"
+                                                        className=" max-w-5xl rounded-lg border"
+                                                    >
+                                                        <ResizablePanel defaultSize={50}>
+                                                            <div className="flex flex-col h-[200px] items-center justify-center p-6 gap-2">
+                                                                <h3 className="font-bold text-xl">About {event.name}:</h3>
+                                                                <p className="font-semibold">{event.description}</p>
                                                             </div>
                                                         </ResizablePanel>
-                                                        <ResizableHandle />
-                                                        <ResizablePanel defaultSize={75}>
-                                                            <div className="flex flex-col h-full items-center justify-center p-6">
-                                                                <span className="font-semibold">START DATE: {convertTimestampToDateAndTime(event.start_date)}</span>
-                                                                <span className="font-semibold">End DATE: {convertTimestampToDateAndTime(event.end_date)}</span>
-                                                            </div>
+                                                        <ResizableHandle withHandle className="text-black" />
+                                                        <ResizablePanel defaultSize={50}>
+                                                            <ResizablePanelGroup direction="vertical">
+                                                            <ResizablePanel defaultSize={25}>
+                                                                <div className="flex h-full items-center justify-center p-6">
+                                                                <span className="font-semibold">VENUE: <Badge variant="secondary" className="">{event.venue}</Badge></span>
+                                                                </div>
+                                                            </ResizablePanel>
+                                                            <ResizableHandle disabled />
+                                                            <ResizablePanel defaultSize={50}>
+                                                                <div className="flex flex-col h-full items-center justify-center p-6 gap-2">
+                                                                    <span className="font-semibold text-left">START DATE: {convertTimestampToDateAndTime(event.start_date)}</span>
+                                                                    <span className="font-semibold text-left">End DATE: {convertTimestampToDateAndTime(event.end_date)}</span>
+                                                                </div>
+                                                            </ResizablePanel>
+                                                            <ResizableHandle disabled />
+                                                            <ResizablePanel defaultSize={25}>
+                                                                <span className="flex flex-row h-full items-center justify-center p-6 gap-2 font-semibold">
+                                                                    Know More: <Info onClick={() => window.open(event.moreinfo)} className=" text-white hover:bg-gray-300 hover:text-black rounded-lg transition-all duration-200 ease-in-out cursor-pointer"/>
+                                                                </span>
+                                                            </ResizablePanel>
+                                                            </ResizablePanelGroup>
                                                         </ResizablePanel>
-                                                        </ResizablePanelGroup>
-                                                    </ResizablePanel>
-                                                </ResizablePanelGroup>
-                                            </DrawerDescription>
-                                            <DrawerFooter className="flex flex-row gap-4 mx-2">
-                                                <Button className="text-black flex-1" variant='secondary'  onClick={() => router.push(`/events/register/${event.name}`)}>
-                                                    Register Now
-                                                </Button>
-                                                <DrawerClose asChild>
-                                                    <Button variant="destructive" className="flex flex-1 justify-center mr-10 bg-[#555555] text-white "><XCircle /></Button>
-                                                </DrawerClose>
-                                            </DrawerFooter>
-                                    </DrawerContent>
-                                </Drawer>
-                            </CardHeader>
-                        </Card>
-                    ))}
+                                                    </ResizablePanelGroup>
+                                                </DrawerDescription>
+                                                <DrawerFooter className="flex flex-row gap-4 mx-2">
+                                                    <Button className="text-black flex-1" variant='secondary'  onClick={() => router.push(`/events/register/${event.name}`)}>
+                                                        Register Now
+                                                    </Button>
+                                                    <DrawerClose asChild>
+                                                        <Button variant="destructive" className="flex flex-1 justify-center mr-10 bg-[#555555] text-white "><XCircle /></Button>
+                                                    </DrawerClose>
+                                                </DrawerFooter>
+                                        </DrawerContent>
+                                    </Drawer>
+                                </CardHeader>
+                            </Card>
+                        ))
+                    )}
                 </div>
             </section>
         </main>
--- app/events/register/[slug]/page.tsx
+++ app/events/register/[slug]/page.tsx
@@ -1,17 +1,159 @@
 'use client'
 import Navbar from "@/app/Navbar";
+import { supabase } from "@/app/supabase.config";
 import Link from "next/link";
-import { useParams } from "next/navigation";
-import { string } from "zod";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogFooter,
+    DialogClose,
+  } from "@/components/ui/dialog"
+import { Router } from "lucide-react";
+
+
+
+interface EventRegistration {
+    name_lead: string;
+    phone_lead: number;
+    email_lead: string;
+    registration_lead: string;
+    name_first: string;
+    phone_first: number;
+    email_first: string ;
+    registration_first: string ;
+    name_second: string | null;
+    registration_second: string | null;
+    name_third: string | null;
+    registration_third: string | null;
+    name_fourth: string | null;
+    registration_fourth: string | null;
+    track_name: string;
+    idea_desc: string;
+    eventid: string | null;
+    gender_lead: string;
+    gender_first: string;
+    gender_second: string;
+    gender_third: string;
+    gender_fourth: string;
+}
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  })
 
 export default function Page() {
-    const params = useParams<{ tag: string; item: string }>();
+    const params = useParams<{ tag: string; item: string, slug: string }>();
+    const [eventid, setEventid] = useState('');
 
-    console.log(params);
+    const router = useRouter();
+
+    useEffect(() => {
+        //get event id from params
+        const getEventId = async () => {
+            let { data: eventslistid, error } = await supabase
+            .from('eventslist')
+            .select('id')
+            .eq('name', params.slug);
+
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            else {
+                if (eventslistid) {
+                    toast.success('Form fetched successfully');
+                    setEventid(eventslistid[0].id || '');
+                    console.log(eventslistid)
+                }
+            }
+        }
+
+        getEventId();
+    }, [])
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          username: "",
+        },
+      })
+     
+      // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+        console.log(values)
+        toast.success('Registered successfully');
+        router.push('/');
+    }
+
     return (
         <main className="flex flex-col w-screen h-full min-h-screen bg-[#090909] text-white">
             <Navbar/>
-
+            <section className="flex flex-col items-center justify-evenly w-full my-6 md:my-12 lg:my-24">
+                <h2 className=" font-mono font-medium text-3xl w-full text-center text-white my-2">Registering for {params.slug}</h2>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-8/12">
+                        <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        
+                        <Dialog>
+                            <DialogTrigger><Button>Submit</Button></DialogTrigger>
+                            <DialogContent className=" bg-[#0E0E0E] text-white">
+                                <DialogHeader>
+                                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                                <DialogDescription>
+                                    This action cannot be undone. This will your final Submission. Changes will not be allowed
+                                </DialogDescription>
+                                </DialogHeader>
+                                <DialogFooter className="flex flex-row w-full gap-2 my-4">
+                                    <DialogClose className="flex-1 w-full"><Button type="button" variant='outline'  className="w-full text-black">Make some Changes</Button></DialogClose>
+                                    <Button type="submit" variant='destructive' className=" flex-shrink">Submit</Button>
+                                </DialogFooter>
+                            </DialogContent>
+                        </Dialog>
+                    </form>
+                </Form>
+            </section>
         </main>
     );
 }

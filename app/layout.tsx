084f8f61995498b22f4f7f823f9e3126225e2fcd
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-02-17 02:24:49 +0530

    set components for modal notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,15 @@ import { Analytics } from '@vercel/analytics/react';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import { useEffect } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -12,6 +21,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  useEffect(() =>{
+    const checkLocalStorage = () => {
+      const value = localStorage.getItem("no_notifications_foundathon");
+    
+      if (!value) {
+        console.log("No value found in local storage for key: ", value);
+        // alert("We are hosting a hackathon! Click here to know more");
+
+      }
+    };
+    setTimeout(() => {
+      checkLocalStorage();
+    }, 5000);
+  },[]);
+
   return (
     <html lang="en">
       <body className={inter.className}>

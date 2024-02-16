349329288f1a2f754579e9bbabd1d55dab077cd2
VijayMakkad <vijaymakkad0104@gmail.com>
2024-02-16 21:27:22 +0530

    final

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -25,7 +25,7 @@ function Footer() {
         </div>
         <div className="p-5">
           <ul>
-            <p className="text-white font-bold text-2xl pb-4">Company</p>
+            <p className="text-white font-bold text-2xl pb-4">Club</p>
             <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
               <Link href="/about">About</Link>
             </li>

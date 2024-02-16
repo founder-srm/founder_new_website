4963b65aca8974f66429139f88ea48f9db2e5625
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-02-17 02:36:49 +0530

    meme on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,36 @@
 import Link from "next/link";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import Image from "next/image";
 
 export default function Page() {
     return (
-        <div>
-        <h1>About</h1>
-        <Link href
-="/">Home</Link>
-        </div>
+        <main className="w-screen h-full flex flex-col bg-[#090909]">
+            <Navbar />
+            <section className="w-full min-h-screen flex flex-col items-center justify-center my-12">
+                <Card className="bg-[#0E0E0E] p-4 text-white shadow-md shadow-gray-400 ">
+                    <CardHeader>
+                        <CardTitle>Under Construction</CardTitle>
+                        <CardDescription>We Are Working on this Page</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Image src="/dj_construction.gif" alt="under construction" width={400} height={400} className="h-[500px] w-auto"  />
+                    </CardContent>
+                    <CardFooter>
+                        <p>We appreciate your patience</p>
+                    </CardFooter>
+                </Card>
+            </section>
+            <Footer />
+        </main>
     );
 }
